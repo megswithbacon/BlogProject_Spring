928235aecad0317dca3ce5d284cad6d2f408136e
Megan <megswithbacon@gmail.com>
2021-03-05 11:14:12 -0600

    NewBlogPost button replacement, link to homepage available, addBlogPost method update, CreateNewBlogPost button

--- src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
@@ -6,22 +6,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class BlogPostController {
     
     @Autowired
     private BlogPostRepository blogPostRepository;
+    private static List<BlogPost> posts = new ArrayList<>();
 
     @GetMapping(value="/")
-    public String index(BlogPost blogPost) {
+    public String index(BlogPost blogPost, Model model) {
+        model.addAttribute("posts", posts);
         return "blogpost/index";
     }
 
     private BlogPost blogPost;
 
-    @PostMapping(value = "/")
+    @GetMapping(value = "/blogposts/new")
+    public String newBlog (BlogPost blogPost) {
+        return "blogpost/new";
+    }
+
+    @PostMapping(value = "/blogposts")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
-        blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+        blogPostRepository.save(blogPost);
+        posts.add(blogPost);
         model.addAttribute("title", blogPost.getTitle());
         model.addAttribute("author", blogPost.getAuthor());
         model.addAttribute("blogEntry", blogPost.getBlogEntry());

8bd91607ca8400b7f5688b02808f3a1b0b7658c3
Megan <megswithbacon@gmail.com>
2021-03-05 12:11:53 -0600

    Nav Bar addition, addBlogPost and Home button available on NavBar, Delete button(no funct) access after adding blogPost

--- src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
@@ -3,20 +3,19 @@ package com.techtalentsouth.TechTalentBlog.BlogPost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class BlogPostController {
-    
+
     @Autowired
     private BlogPostRepository blogPostRepository;
     private static List<BlogPost> posts = new ArrayList<>();
 
-    @GetMapping(value="/")
+    @GetMapping(value = "/")
     public String index(BlogPost blogPost, Model model) {
         model.addAttribute("posts", posts);
         return "blogpost/index";
@@ -25,7 +24,7 @@ public class BlogPostController {
     private BlogPost blogPost;
 
     @GetMapping(value = "/blogposts/new")
-    public String newBlog (BlogPost blogPost) {
+    public String newBlog(BlogPost blogPost) {
         return "blogpost/new";
     }
 
@@ -37,5 +36,11 @@ public class BlogPostController {
         model.addAttribute("author", blogPost.getAuthor());
         model.addAttribute("blogEntry", blogPost.getBlogEntry());
         return "blogpost/result";
+    }
+
+    @RequestMapping(value = "/blogposts/{id}", method = RequestMethod.DELETE)
+    public String deletePostWithId(@PathVariable Long id, BlogPost blogPost) {
+        blogPostRepository.deleteById(id);
+        return "blogpost/index";
     }
 }
